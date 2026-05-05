39b630f819abf86c60820aa17529d67df5ed45df
Rotremma <rotrndubuisiemmanuel@gmail.com>
2026-05-05 15:11:13 -0400

    compared phones for importing

---
+++ gitphone1.py
@@ -0,0 +1,13 @@
+def phone():
+    if "nokia" == "samsung" and "iphone" == "itel":
+        pass
+    elif "nokia" == "iphone*":
+        pass
+    elif "iphone" == "samsung":
+        pass
+    else:
+        print("import")
+
+phone()
+
+
